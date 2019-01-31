a367f149061c4bba9ad8eba3fa5e6ede134e7036
sirishamca <47160687+sirishamca@users.noreply.github.com>
2019-01-31 13:58:32 -0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"getStockData"
+	"github.com/gorilla/mux"
+	"github.com/sirishamca/stockrestapi/config"
+	"github.com/sirishamca/stockrestapi/getStockData"
+	"github.com/sirishamca/stockrestapi/srslogger"
+	"go/build"
+	"log"
+	"net/http"
+)
+
+var l srslogger.Logger
+
+func handleRequests() {
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/stock/{symbol}", returnStockData)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func returnStockData(w http.ResponseWriter, r *http.Request) {
+	queryValues := r.URL.Query()
+	queryParam := queryValues.Get("stock_exchange")
+
+	vars := mux.Vars(r)
+	key := vars["symbol"]
+	data, err := getStockData.GetStockData(key, queryParam, l)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	} else {
+		w.Write(data)
+	}
+}
+
+func main() {
+	var logpath = build.Default.GOPATH + config.LOGFILEPATH
+	l.Init(logpath)
+	handleRequests()
+}
